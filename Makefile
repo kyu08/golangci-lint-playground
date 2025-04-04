a58ed183de8cb8720ac6d1a719b7be4acc0ac41b
kyu08 <49891479+kyu08@users.noreply.github.com>
2025-04-05 00:42:38 +0900

    nilnil

--- Makefile
+++ Makefile
@@ -4,7 +4,4 @@ run:
 lint:
 	GOTOOLCHAIN=go1.24.0 go run github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.0.2 run  ./... --config ./.golangci.yml
 
-lint-fix:
-	GOTOOLCHAIN=go1.24.0 go run github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.0.2 run  ./... --config ./.golangci.yml --fix
-
-.PHONY: run lint lint-fix
+.PHONY: run lint
--- main.go
+++ main.go
@@ -5,20 +5,60 @@ import (
 )
 
 func main() {
-	u := user{name: "John"}
-	fmt.Printf("u.greet(): %v\n", u.greet())
-	u.update("Doe")
-	fmt.Printf("u.greet(): %v\n", u.greet())
+	userID := "123"
+	user, address, err := searchUserAndAddressByUserID(userID)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("User: %+v\n", user)
+	fmt.Printf("Address: %+v\n", address)
 }
 
-type user struct {
-	name string
+type (
+	user struct {
+		id     string
+		name   string
+		status string
+	}
+	address struct {
+		country       string
+		city          string
+		StreetAddress string
+	}
+)
+
+func searchUserAndAddressByUserID(userID string) (*user, *address, error) {
+	user, err := getUser(userID)
+	if err != nil {
+		return nil, nil, err
+	}
+	if user.status != "active" {
+		// This should be return nil, nil, ErrNotFound.
+		return nil, nil, nil
+	}
+
+	address, err := getAddress()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return user, address, nil
 }
 
-func (u user) greet() string {
-	return fmt.Sprintf("I'm %s", u.name)
+func getUser(userID string) (*user, error) {
+	return &user{
+		id:     userID,
+		name:   "john",
+		status: "active",
+	}, nil
 }
 
-func (u *user) update(after string) {
-	u.name = after
+func getAddress() (*address, error) {
+	return &address{
+		country:       "Japan",
+		city:          "Tokyo",
+		StreetAddress: "1-1",
+	}, nil
 }
+
